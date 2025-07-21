18cdfe2f7f45ef5e7ed5e44b0923575f90f26d5a
rainitesh2811 <rainitesh513@gmail.com>
2025-07-21 10:59:55 +0530

    Update server_capture_payment.js

--- server_capture_payment.js
+++ server_capture_payment.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: 'https://unitedlearning.in'
+  origin: 'https://unitedlearning.in/'
 }));
 
 app.use(express.json());
